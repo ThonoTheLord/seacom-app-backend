9a46b36bfa96705bacdb4863322aa69a92a1c096
Caff-Core <mkay.py@gmail.com>
2026-01-03 19:20:47 +0200

    Add BaseDB model

---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from .base import BaseDB
+
+__all__ = ["BaseDB"]
---
+++ app/models/base.py
@@ -0,0 +1,43 @@
+from uuid import UUID, uuid4
+from sqlmodel import SQLModel, Column, DateTime, Field
+from abc import ABC
+from datetime import datetime
+
+from app.utils.funcs import utcnow
+
+
+class BaseDB(SQLModel, ABC):
+    """"""
+
+    id: UUID = Field(
+        default_factory=uuid4,
+        primary_key=True,
+        description="unique database identifier",
+        schema_extra={"examples": str(uuid4())},
+    )
+    created_at: datetime = Field(
+        default_factory=utcnow,
+        sa_column=Column(DateTime(timezone=True), nullable=False),
+        description="date and time with a timezone the record was created.",
+        schema_extra={"examples": {str(utcnow())}},
+    )
+    updated_at: datetime = Field(
+        default_factory=utcnow,
+        sa_column=Column(DateTime(timezone=True), nullable=False),
+        description="date and time with a timezone the record was last updated.",
+        schema_extra={"examples": {str(utcnow())}},
+    )
+    deleted_at: datetime | None = Field(
+        default=None,
+        sa_column=Column(DateTime(timezone=True)),
+        description="date and time with a timezone the record was deleted.",
+        schema_extra={"examples": {str(utcnow()), None}},
+    )
+
+    def touch(self) -> None:
+        """Update the updated_at to the current date and time."""
+        self.updated_at = utcnow()
+    
+    def soft_delete(self) -> None:
+        """Mark the record as deleted."""
+        self.deleted_at = utcnow()
